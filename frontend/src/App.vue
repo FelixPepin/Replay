57e4ec482def88f5b45fd9a532c0da72b83c3999
Charles Gilbert <chgil001@edu.cegepgarneau.ca>
2026-02-25 17:17:01 -0500

    Fix (Page d'accueil)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,39 +15,37 @@ function deconnecter() {
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg">
-      <a class="navbar-brand" href="/">Replay</a>
-      <RouterLink to="/achat" class="btn btn-primary p-2 text-black m-2">Achat</RouterLink>
-      <RouterLink to="/vendre" class="btn btn-primary p-2 text-black m-2">Vendre un jeu</RouterLink>
-      <RouterLink to="/register" class="btn btn-primary p-2 text-black m-2">Inscription</RouterLink>
-      <button v-show="auth.estConnecte" id="deconnexion" @click="deconnecter" class="btn btn-info p-2 text-black m-2">Se
-        déconnecter</button>
-    </nav>
-  </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 shadow-sm">
+      <div class="container-fluid">
+        <a class="navbar-brand fw-bold fs-3 text-warning" href="/">RePlay</a>
 
-  <body>
-    <div id="carouselExample" class="carousel slide">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="..." class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
+        <div class="d-flex align-items-center">
+          <RouterLink to="/achat" class="nav-link text-white me-3">Acheter</RouterLink>
+          <RouterLink to="/vendre" class="nav-link text-white me-3">Vendre</RouterLink>
+
+          <div class="ms-2 d-flex gap-2">
+            <RouterLink v-if="!auth.estConnecte" to="/register" class="btn btn-outline-warning btn-sm">
+              Inscription
+            </RouterLink>
+            <button v-if="auth.estConnecte" @click="deconnecter" class="btn btn-info btn-sm">
+              Se déconnecter
+            </button>
+          </div>
         </div>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-  </body>
-
+    </nav>
+  </header>
   <RouterView />
 </template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+.nav-link:hover {
+  color: #ffca28 !important;
+}
+</style>
