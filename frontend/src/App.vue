7c553769ced745dbd13a842d0bb27cb6e71d30dd
Charles Gilbert <chgil001@edu.cegepgarneau.ca>
2026-02-25 12:10:22 -0500

    Feat (Déconnexion)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref , computed} from 'vue';
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+const auth = useAuthStore()
+const router = useRouter()
+const estConnecte = computed(() => !!localStorage.getItem('token'))
+
+
+function deconnecter() {
+  auth.deconnecter()
+    router.push('/login')
+}
 </script>
 
 <template>
   <header>
       <nav class="navbar navbar-expand">
-        <RouterLink to="/vendre" class="btn btn-primary p-2 text-black">Vendre un jeu</RouterLink>
-        <RouterLink to="/register" class="btn btn-primary p-2 text-black">Inscription</RouterLink>
+        <RouterLink to="/vendre" class="btn btn-primary p-2 text-black m-2">Vendre un jeu</RouterLink>
+        <RouterLink to="/register" class="btn btn-primary p-2 text-black m-2">Inscription</RouterLink>
+        <button v-show="auth.estConnecte" id="deconnexion" @click="deconnecter" class="btn btn-info p-2 text-black m-2">Se déconnecter</button>
       </nav>
   </header>
 
   <RouterView />
 </template>
+
 
