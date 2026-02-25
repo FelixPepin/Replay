003341cbaf2e86795dda608e350694b23e160410
Charles Gilbert <chgil001@edu.cegepgarneau.ca>
2026-02-25 13:29:04 -0500

    Fix navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref , computed} from 'vue';
+import { ref, computed } from 'vue';
 import { RouterLink, RouterView, useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 const auth = useAuthStore()
@@ -9,20 +9,21 @@ const estConnecte = computed(() => !!localStorage.getItem('token'))
 
 function deconnecter() {
   auth.deconnecter()
-    router.push('/login')
+  router.push('/login')
 }
 </script>
 
 <template>
   <header>
-      <nav class="navbar navbar-expand">
-        <RouterLink to="/vendre" class="btn btn-primary p-2 text-black m-2">Vendre un jeu</RouterLink>
-        <RouterLink to="/register" class="btn btn-primary p-2 text-black m-2">Inscription</RouterLink>
-        <button v-show="auth.estConnecte" id="deconnexion" @click="deconnecter" class="btn btn-info p-2 text-black m-2">Se déconnecter</button>
-      </nav>
+    <nav class="navbar navbar-expand-lg">
+      <a class="navbar-brand" href="/">Replay</a>
+      <RouterLink to="/achat" class="btn btn-primary p-2 text-black m-2">Achat</RouterLink>
+      <RouterLink to="/vendre" class="btn btn-primary p-2 text-black m-2">Vendre un jeu</RouterLink>
+      <RouterLink to="/register" class="btn btn-primary p-2 text-black m-2">Inscription</RouterLink>
+      <button v-show="auth.estConnecte" id="deconnexion" @click="deconnecter" class="btn btn-info p-2 text-black m-2">Se
+        déconnecter</button>
+    </nav>
   </header>
 
   <RouterView />
 </template>
-
-
