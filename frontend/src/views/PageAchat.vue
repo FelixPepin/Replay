ecc8dd1d8f5ed78b7347534d9cab02949581ddcf
Charles Gilbert <chgil001@edu.cegepgarneau.ca>
2026-03-21 15:32:57 -0400

    Fix (Affichage des boutons)

--- frontend/src/views/PageAchat.vue
+++ frontend/src/views/PageAchat.vue
@@ -3,6 +3,9 @@
     <div class="container mt-5">
       <h1>Liste de jeux en vente</h1>
     </div>
+    <div v-if="notif.message" :class="`alert alert-${notif.type}`">
+      {{ notif.message }}
+    </div>
 
     <div class="row">
       <div v-for="jeu in jeux" :key="jeu.Id" class="col-lg-6 mb-3">
@@ -14,6 +17,12 @@
             <p class="card-text">Vendeur : {{ jeu.NomUtilisateur }}</p>
             <p class="card-text">Livraison : {{ jeu.TypeLivraison }}</p>
             <p class="card-text">Paiement : {{ jeu.TypePaiement }}</p>
+            <RouterLink v-if="!jeu.estVendu" :to="`/acheter/${jeu.Id}`" class="btn btn-success rounded-2">
+              Acheter
+            </RouterLink>
+            <button v-else class="btn btn-danger rounded-2" disabled>
+              Vendu
+            </button>
           </div>
         </div>
       </div>
@@ -24,16 +33,16 @@
 import { computed, onMounted, ref } from 'vue';
 import axios from 'axios'
 import { useAuthStore } from '@/stores/auth';
+import { useNotifStore } from '@/stores/notif';
 const jeux = ref([])
 const auth = useAuthStore()
+const notif = useNotifStore()
 
 
 onMounted(async () => {
   try {
     const response = await axios.get('/api/ventes')
-    console.log('type:', typeof response.data)
-    console.log('données:', response.data)
-
+    console.log(response.data[0])
     jeux.value = Array.isArray(response.data) ? response.data : []
   } catch (err) {
     console.error('Erreur:', err)
