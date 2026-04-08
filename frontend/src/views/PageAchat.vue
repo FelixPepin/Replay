d48db9527d5854c1239d796c7c14d83eee954c60
Charles Gilbert <chgil001@edu.cegepgarneau.ca>
2026-04-08 18:01:19 -0400

    Feat (Recherche jeux)

--- frontend/src/views/PageAchat.vue
+++ frontend/src/views/PageAchat.vue
@@ -7,8 +7,13 @@
       {{ notif.message }}
     </div>
 
+    <div class="d-flex justify-content-between align-items-center mb-3">
+      <div class="col-md-4">
+        <input v-model="recherche" class="form-control me-2" type="search" placeholder="Nom du jeu" aria-label="Search">
+      </div>
+    </div>
     <div class="row">
-      <div v-for="jeu in jeux" :key="jeu.Id" class="col-lg-6 mb-3">
+      <div v-for="jeu in jeuxTriees" :key="jeu.Id" class="col-lg-6 mb-3">
         <div class="card h-100">
           <img class="card-img-top w-100" :src="`/static/images/ajouts/${jeu.Photo}`" />
           <div class="card-body">
@@ -37,8 +42,18 @@ import { useNotifStore } from '@/stores/notif';
 const jeux = ref([])
 const auth = useAuthStore()
 const notif = useNotifStore()
+const recherche = ref('')
+
+const jeuxTriees = computed(() => {
+  let liste = [...jeux.value]
+  if (liste.length === 0) return []
+
+  if (recherche.value.trim() !== '')
+    liste = liste.filter(jeu => jeu.NomJeu.toLowerCase().includes(recherche.value.toLowerCase()))
 
 
+  return liste
+})
 onMounted(async () => {
   try {
     const response = await axios.get('/api/ventes')
