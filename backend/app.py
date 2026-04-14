77ae8d462467e6cf3eed6ab7f193c52d0297c855
Félix Pépin <fepep002@edu.cegepgarneau.ca>
2026-04-14 12:04:48 -0400

    Page louer jeu fait.

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 from routes.authentification import bp_auth
 from routes.location import bp_location
 from routes.vente import bp_vente
+from routes.reservations import bp_reservation;
 
 load_dotenv()
 
@@ -16,6 +17,7 @@ CORS(app)
 app.register_blueprint(bp_auth, url_prefix='/api')
 app.register_blueprint(bp_vente, url_prefix='/api')
 app.register_blueprint(bp_location, url_prefix='/api')
+app.register_blueprint(bp_reservation, url_prefix='/api')
 
 app.config['MORCEAUX_VERS_AJOUTS'] = ['static', 'images', 'ajouts']
 app.config['ROUTE_VERS_AJOUTS'] = "/".join(app.config['MORCEAUX_VERS_AJOUTS'])
